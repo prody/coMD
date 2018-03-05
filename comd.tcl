c3531e21521251307473dd04fc5585411da99b46
Amaya Jimenez <ajimoreno@gmail.com>
2018-03-05 19:34:02 +0100

    Removed CPU checking code because not necessary and not certain

--- comd.tcl
+++ comd.tcl
@@ -1430,35 +1430,6 @@ proc comd_tk {} {
   ::comd::comdgui
 }
 
-proc numberOfCPUs {} {
-    # Windows puts it in an environment variable
-    global tcl_platform env
-    if {$tcl_platform(platform) eq "windows"} {
-        return $env(NUMBER_OF_PROCESSORS)
-    }
-
-    # Check for sysctl (OSX, BSD)
-    set sysctl [auto_execok "sysctl"]
-    if {[llength $sysctl]} {
-        if {![catch {exec $sysctl -n "hw.ncpu"} cores]} {
-            return $cores
-        }
-    }
-
-    # Assume Linux, which has /proc/cpuinfo, but be careful
-    if {![catch {open "/proc/cpuinfo"} f]} {
-        set cores [regexp -all -line {^processor\s} [read $f]]
-        close $f
-        if {$cores > 0} {
-            return $cores
-        }
-    }
-
-    # No idea what the actual number of cores is; exhausted all our options
-    # Fall back to returning 1; there must be at least that because we're running on it!
-    return 1
-}
-
 if { $argc < 3 } {
   puts "comd.tcl requires at least two arguments: filenames for the starting PDBs."
   puts "Please provide the same filename twice to calculate a random walk "
